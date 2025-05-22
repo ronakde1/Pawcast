641a639b63f66aa9b31dbd12ae8bb8c7d2110456
ronakye <ronakde84@gmail.com>
2025-05-22 15:00:32 +0100

    layout changes

--- pawcastapp/app/(tabs)/_layout.tsx
+++ pawcastapp/app/(tabs)/_layout.tsx
@@ -15,17 +15,48 @@ export default function TabsLayout() {
         <Ionicons 
         name = {focused? "home-sharp": "home-outline"} 
         size = {30}
+        color = {color}
         />
       }}
     />
     <Tabs.Screen name="details" 
-    options = {{headerTitle: "Details"}}
+    options = {{headerTitle: "Details",
+      tabBarIcon: ({focused, color}) => 
+        <Ionicons 
+        name = {focused? "ellipsis-vertical-circle-sharp": "ellipsis-vertical-circle-outline"} 
+        size = {30}
+        color = {color}
+        />
+
+
+    }}
     />
     <Tabs.Screen name="information" 
-    options = {{headerTitle: "Information"}}
+    options = {{headerTitle: "Information", 
+      tabBarIcon: ({focused, color}) => 
+        <Ionicons 
+        name = {focused? "information-circle-sharp": "information-circle-outline"} 
+        size = {30}
+        color = {color}
+        />
+
+    }}
     />
     <Tabs.Screen name="register" 
-    options = {{headerTitle: "register"}}
+     options = {{headerTitle: "register", 
+      tabBarIcon: ({focused, color}) => 
+        <Ionicons 
+        name = {focused? "document-sharp": "document-outline"} 
+        size = {30}
+        color = {color}
+        />
+    }}
     />
+    <Tabs.Screen name="weather" 
+    options={{
+      tabBarStyle: { display: 'none' },
+      tabBarButton: (props) => null,
+    }}/>
 </Tabs>;
 }
+//Details, information, register
